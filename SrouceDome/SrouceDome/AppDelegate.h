5b3013e9e4cfc9169e79b22974f20eb0a71aaaf3
Refordom <18779884209@163.com>
2017-03-20 13:59:13 +0800

    第一个demo

---
+++ SrouceDome/SrouceDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SrouceDome
+//
+//  Created by Refordom on 17/3/20.
+//  Copyright © 2017年 Refordom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SrouceDome/SrouceDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SrouceDome
+//
+//  Created by Refordom on 17/3/20.
+//  Copyright © 2017年 Refordom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
